5b9156ead68fe144c241690456530facd99c91ee
codesmagnus <magnusbruus13@gmail.com>
2026-02-26 09:35:11 +0100

    Update naming_exe.cpp

--- naming_exe.cpp
+++ naming_exe.cpp
@@ -1,6 +1,7 @@
-class sum {
-  public:
-      int d(int* a, int size) {
+class sum 
+{
+  public: int d(int* a, int size) 
+      {
           int s = 0;
           for (int i = 0; i < size; i++)
               s += a[i];
