c6f780f0b3a198074fd5a795190f0939bd5d4d68
gulleh5002-spec <gulleh5002@gmail.com>
2026-03-05 14:16:51 +0100

    Add files via upload

---
+++ pyarmcode.py
@@ -0,0 +1,35 @@
+import rtde_control
+import rtde_receive
+import math
+
+class RobotMovements:
+    def __init__(self, ip):
+        self.ip = ip
+        self.rtde_c = rtde_control.RTDEControlInterface(ip)
+        self.rtde_r = rtde_receive.RTDEReceiveInterface(ip)
+        print("forbundet til:" + ip)
+
+    def get_position(self):
+        pose = self.rtde_r.getActualTCPPose()
+        return pose
+    
+    def move_to(self, x, y ,z):
+        punkt = [x, y, z, 0, 3.14, 0]
+        self.rtde_c.moveL(punkt, 0.5, 0.5)
+        print(f"Flyttet til: X={x}, Y={y}, Z={z}")
+        
+    def home(self):
+        self.rtde_c.moveJ([0, -1.57, 1.57, 0, 0, 0], 0.5, 0.5)
+        print("home")
+
+    def rotate_tool():
+        pass
+
+
+
+
+better = RobotMovements("192.168.1.11")
+
+better.home()
+
+
