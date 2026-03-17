b922c40786a14d95f56453ab87f2a201fca4ec08
mihan23 <mihan23@student.sdu.dk>
2026-03-17 09:10:48 +0100

    Rettet sum

--- naming_exe.cpp
+++ naming_exe.cpp
@@ -3,7 +3,7 @@ class Sum
   public: 
   int beregnSum(int* tal, int size) 
       {
-          int sum = 0;
+          constexpr int sum = { 0 }; // Brug constexpr og const til lokale variabler
           for (int i = 0; i < size; i++)
           sum += tal[i];
           return sum;
