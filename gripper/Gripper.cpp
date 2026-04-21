9233daf19daa3d9c1dc06efc88341f4450970f13
apram2575-oss <apram2575@gmail.com>
2026-04-21 14:12:37 +0200

    With sensor functions

--- gripper/Gripper.cpp
+++ gripper/Gripper.cpp
@@ -1,9 +1,10 @@
 #include "Gripper.h"
 
-Gripper::Gripper(int dirPin1, int dirPin2)
+Gripper::Gripper(int dirPin1, int dirPin2, int currentPin)
 {
   _dirPin1 = dirPin1;
   _dirPin2 = dirPin2;
+  _currentPin = currentPin;
 
 }
 
@@ -12,6 +13,7 @@ void Gripper::begin()
 
   pinMode(_dirPin1, OUTPUT); 
   pinMode(_dirPin2, OUTPUT);
+  pinMode(_currentPin, INPUT);
   stop();
 
 }
@@ -33,6 +35,45 @@ void Gripper::stop()
 {
   digitalWrite(_dirPin1, LOW);
   digitalWrite(_dirPin2, LOW);
+}
+
+float Gripper::readCurrent()
+{
+  int raw = analogRead(_currentPin);
+
+  float voltage = raw * (3.3f / 4095.0f);
+
+  // V_actual = V_measured × (R1+R2)/R2 = V_measured × 30k/20k = × 1.5
+  voltage = voltage * 1.5f;
+
+    // ACS712 05B: 0A = 2.5V, sensitivity = 185mV/A
+  float current = (voltage - 2.5f) / 0.185f;
+
+  return current; // Measured in Amps
+}
+
+
+
+void Gripper::closeUntilGrip(int speed, float threshold)
+{
+  close(speed);
+  while (true)
+  {
+    float current = readCurrent();
+    Serial.print("Current: ");
+    Serial.print(current, 3);
+    Serial.println(" A");
+
+    if (abs(current) >= threshold)
+    {
+      stop();
+      Serial.println("Object gripped!");
+      break;
+
+    }
+    delay(10);
+
+  }
 }
 
 
