ad87027b58a4d5c66b794f5711ac3f14644b9f90
apram2575-oss <apram2575@gmail.com>
2026-04-21 14:12:59 +0200

    With sensor functions

--- gripper/Gripper.h
+++ gripper/Gripper.h
@@ -5,14 +5,16 @@
 class Gripper 
 {
 public:
-Gripper(int dirPin1, int dirPin2);
+Gripper(int dirPin1, int dirPin2, int currentPin);
 void begin();
-void close(int speed = 150);
-void open(int speed = 150);
+void close(int speed = 200);
+void open(int speed = 200);
 void stop();
+void closeUntilGrip(int speed = 200, float threshold = 0.4);
+float readCurrent();
 
 private: 
 int _dirPin1;
 int _dirPin2;
-
+int _currentPin;
 };
