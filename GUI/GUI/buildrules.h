104b1fc9d171b07f5b0eee3e088221506ac52d24
mihan23 <mihan23@student.sdu.dk>
2026-04-28 14:32:19 +0200

    Update buildrules.h

--- GUI/GUI/buildrules.h
+++ GUI/GUI/buildrules.h
@@ -7,6 +7,7 @@
 class BuildRules
 {
 public:
+    // Håndtere om man kan fjerne eller placere en klods
     static bool canPlaceBlockAt(const RobotWorkspaceData& data, const GridPosition& position);
     static bool canRemoveBlockAt(const RobotWorkspaceData& data, const GridPosition& position);
 
