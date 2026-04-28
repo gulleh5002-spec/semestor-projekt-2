c6a52da33389f7cf79e6d413f63613fb5e9f2c7a
mihan23 <mihan23@student.sdu.dk>
2026-04-28 11:15:19 +0200

    Refactor og kode tekst i Gridrender.h

--- GUI/GUI/gridposition.h
+++ GUI/GUI/gridposition.h
@@ -1,11 +1,13 @@
 #ifndef GRIDPOSITION_H
 #define GRIDPOSITION_H
 
+
+// Simpel datatype for GridPosition
 struct GridPosition
 {
-    int x = 0;
-    int y = 0;
-    int z = 0;
+    int x = {0};
+    int y = {0};
+    int z = {0};
 };
 
 #endif // GRIDPOSITION_H
