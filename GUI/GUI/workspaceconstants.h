b00db0c4a2024b55b0f0eae63529d1bf7f7d954e
mihan23 <mihan23@student.sdu.dk>
2026-04-28 15:58:43 +0200

    Update workspaceconstants.h

--- GUI/GUI/workspaceconstants.h
+++ GUI/GUI/workspaceconstants.h
@@ -7,6 +7,8 @@ namespace WorkspaceConstants
     inline constexpr int firstLayer{0};
     inline constexpr int defaultGridWidth{4};
     inline constexpr int defaultGridHeight{5};
+
+    // TODO: Tilføj max og minimum grænser
 }
 
 #endif // WORKSPACECONSTANTS_H
