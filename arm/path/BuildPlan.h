9a62bb5e5b48928cbd1bda3510ed2a8de8fc9c89
mihan23 <mihan23@student.sdu.dk>
2026-05-07 14:34:39 +0200

    Add robot build plan data types

---
+++ arm/path/BuildPlan.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct WorkspaceSize
+{
+    int width = 0;
+    int height = 0;
+    int layerCount = 0;
+    bool isCreated = false;
+};
+
+struct BuildPlanBlock
+{
+    int id = 0;
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    std::string type;
+};
+
+struct BuildPlan
+{
+    WorkspaceSize workspace;
+    std::vector<BuildPlanBlock> blocks;
+};
