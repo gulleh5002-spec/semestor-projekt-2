a14e1cdc9c5cdca2f66a23ef16f1a47d186b799f
mihan23 <mihan23@student.sdu.dk>
2026-05-08 13:04:13 +0200

    Listed indexing varables

--- arm/path/BuildPlan.h
+++ arm/path/BuildPlan.h
@@ -5,18 +5,18 @@
 
 struct WorkspaceSize
 {
-    int width = 0;
-    int height = 0;
-    int layerCount = 0;
+    int width = {0};
+    int height = {0};
+    int layerCount = {0};
     bool isCreated = false;
 };
 
 struct BuildPlanBlock
 {
-    int id = 0;
-    int x = 0;
-    int y = 0;
-    int z = 0;
+    int id = {0};
+    int x = {0};
+    int y = {0};
+    int z = {0};
     std::string type;
 };
 
