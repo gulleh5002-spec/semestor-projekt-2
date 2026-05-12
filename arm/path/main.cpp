c8a5915255385f4c9b8f02956934c983c0ad2b71
mihan23 <mihan23@student.sdu.dk>
2026-05-12 17:59:08 +0200

    Oprydning i main.cpp

--- arm/path/main.cpp
+++ arm/path/main.cpp
@@ -30,60 +30,6 @@ bool hasArgument(int argc, char* argv[], const std::string& argument)
     }
 
     return false;
-    RobotArm gulle("192.168.1.100", 1, 1.0);
-    //RobotArm magnum("127.0.0.1", 1.0, 1.0);
-    std::vector<double> point1 = { 0.4, 0.4, 0.2, 3.14, 0.0, 0.0 };
-    std::vector<double> point2 = { 0.2, 0.2, 0.2, 3.14, 0.0, 0.0 };
-    //gulle.moveblock(point1, point2);
-    //gulle.moveblock(point1, point2);
-    
-    //gulle.movetool({ 0.4, 0.4, 0.15, 3.14, 0.0, 0.0 }, 0.5, 0.5, {-0.2 ,-0.2 ,0, 0, 0, 2.3732 });
-    //gulle>.movetool({ 0.4, 0, 0.5, 3.14, 0.0, 0.0 });
-    //gulle.home();
-    std::vector<Block> takeblocks = 
-    {
-    Block(1, {1, 2, 0}),
-    Block(1, {2, 2, 0}),
-    Block(1, {3, 2, 0}),
-    Block(1, {1, 0, 0}),
-    Block(1, {2, 0, 0}),
-    Block(1, {3, 0, 0})
-    };
-
-    std::vector<Block> placeblocks = 
-    {
-    Block(1, {0, 3, 0}),
-    Block(1, {1, 3, 0}),
-    Block(1, {2, 3, 0}),
-    Block(1, {0, 3, 1}),
-    Block(1, {1, 3, 1}),
-    Block(1, {2, 3, 1}),
-    //Block(1, {0, 2, 2})
-    };
-   
-
-
-    Grid place(40, 40, 100, {0.2 ,0.2 ,0, 0, 0, 0});
-    Grid take(40, 40, 100, {0.6 ,0.2, 0, 0, 0, 0});
-    
-    //structur.placeBlock(placeblocks);
-    //gulle.drop();
-    gulle.build(take, place, placeblocks, takeblocks);
-
-     
-    //gulle.drop();
-    //gulle.moveToGridPos(place, takeblocks[1]);
-
-    // take skal ganges på place evt lav en frame med kun rotaton og igne translation der efter gange identits matrice med translation eller trai på den
-    // evt lav en test for at tjekke siguleretet
-
-   
-    //gulle.moveToGridPos(take, placeblocks[0]);
-    //gulle.take();
-
-    return 0;
-
-    
 }
 
 std::optional<std::filesystem::path> buildPlanPathFromArguments(int argc, char* argv[])
@@ -94,11 +40,7 @@ std::optional<std::filesystem::path> buildPlanPathFromArguments(int argc, char* 
         }
 
         const std::string argument = argv[i];
-/*
-        For manuel test med hardcoding uden json
-        kør .\arm\path\build\Debug\RobotArm.exe --manual-test
-        i powershell Terminal
-*/
+
         if (argument != "--dry-run"
             && argument != "--execute"
             && argument != "--manual-test" 
@@ -216,6 +158,11 @@ int runBuildPlanExecute(int argc, char* argv[])
     return 0;
 }
 
+/*
+    For manuel test med hardcoding uden json
+    kør .\arm\path\build\Debug\RobotArm.exe --manual-test
+    i powershell Terminal
+*/
 int runManualRobotTest()
 {
     RobotArm robot(defaultRobotIp, 1.0, 1.0);
@@ -223,11 +170,19 @@ int runManualRobotTest()
     std::vector<Block> takeBlocks = {
         Block(1, {1, 2, 0}),
         Block(1, {2, 2, 0}),
-        Block(1, {3, 3, 0}),
+        Block(1, {3, 2, 0}),
+        Block(1, {1, 0, 0}),
+        Block(1, {2, 0, 0}),
+        Block(1, {3, 0, 0}),
     };
 
     std::vector<Block> placeBlocks = {
         Block(1, {0, 3, 0}),
+        Block(1, {1, 3, 0}),
+        Block(1, {2, 3, 0}),
+        Block(1, {0, 3, 1}),
+        Block(1, {1, 3, 1}),
+        Block(1, {2, 3, 1}),
     };
 
     Grid place(40, 40, 100, {0.2, 0.2, 0, 0, 0, 0});
