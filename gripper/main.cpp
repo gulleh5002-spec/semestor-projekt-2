6187068b88398f3da591b338bfaa1e36aab8f209
apram2575-oss <apram2575@gmail.com>
2026-05-17 11:31:44 +0200

    Update main.cpp

--- gripper/main.cpp
+++ gripper/main.cpp
@@ -1,7 +1,7 @@
 #include "Gripper.h"
 
 // Pins: PWM, Direction1, Direction2
-// Adjust these to match your wiring on the Pico
+
 Gripper gripper(0, 1);
 
 
@@ -9,7 +9,7 @@ Gripper gripper(0, 1);
 
 void setup() 
 {
-  // put your setup code here, to run once:
+ 
   Serial.begin(115200);
   gripper.begin();
   Serial.println("Gripper ready.");
@@ -17,7 +17,6 @@ void setup()
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
 
   Serial.println("Closing...");
   gripper.close(150);   // speed 0-255
